657436da4dc337e4866df65dcd373483fd5b203b
Daniel Ruiz <druizca@mercadona.es>
2019-06-04 01:39:27 +0200

    Implement basic roll method. Calculate basic bowling game with no spares nor strikes

--- game.go
+++ game.go
@@ -4,7 +4,16 @@ type game struct {
 	score int
 }
 
+// New initializes the game struct.
 func New() game {
 	game := game{}
 	return game
+}
+
+func (game *game) Score() int {
+	return game.score
+}
+
+func (game *game) Roll(pinsDown int) {
+	game.score += pinsDown
 }
--- game_test.go
+++ game_test.go
@@ -8,3 +8,18 @@ func TestGameStartsWithZeroScore(t *testing.T) {
 		t.Errorf("The initial score of a game is zero. Got %d instead.", game.score)
 	}
 }
+
+func TestCalculatesGameScoreWhenNoSparesNorStrikes(t *testing.T) {
+	rolls := []int{5, 3, 4, 4, 6, 2, 7, 1, 1, 7, 2, 6, 3, 5, 4, 4, 8, 0, 0, 8}
+	expectedScore := 80
+	game := New()
+
+	for _, pinsDown := range rolls {
+		game.Roll(pinsDown)
+	}
+
+	score := game.Score()
+	if score != expectedScore {
+		t.Errorf("Expected score to be %d. Actual is %d.", expectedScore, score)
+	}
+}
